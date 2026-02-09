7ba5a663b1ba7778873177b7074ea1e6ac12af9d
Thami Nkosivelaphi <nkosivelaphi.t@gmail.com>
2026-02-09 11:12:59 +0200

    Update run configuration

--- run.py
+++ run.py
@@ -4,5 +4,7 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
+    debug = os.getenv("DEBUG", "False").lower() == "true"
     port = int(os.getenv("PORT", "5000"))
-    app.run(host="0.0.0.0", port=port)
+
+    app.run(host="0.0.0.0", port=port, debug=debug)

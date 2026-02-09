5cce72c40c3b317532df891aadc836f183c20117
Thami Nkosivelaphi <nkosivelaphi.t@gmail.com>
2026-02-09 12:07:11 +0200

    Auto-create tables on startup

--- start.sh
+++ start.sh
@@ -3,4 +3,8 @@ set -e
 
 python -m pip install -r requirements.txt --target ./vendor
 export PYTHONPATH=./vendor
+
+# Create tables on startup (safe if they already exist)
+python -c "from app import create_app, db; app=create_app(); app.app_context().push(); db.create_all(); print('db-ready')"
+
 python run.py
